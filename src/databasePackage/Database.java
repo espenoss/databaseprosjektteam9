7af9c01fd25f2f844575db8f725f1e4e47a2447a
marith1 <marith1@student.hist.no>
2016-03-16 14:45:20 +0100

    databasekonstruktør

--- src/databasePackage/Database.java
+++ src/databasePackage/Database.java
@@ -1,8 +1,19 @@
 package databasePackage;
 
 public abstract class Database {
-	//TEST marit :)
+	private final String dbDriver;
+	private final String dbName;
+	
+	public Database(String dbDriver, String dbName){
+		this.dbDriver = dbDriver;
+		this.dbName = dbName;
+	}
+	
+	
+	
 	public abstract boolean registerUser(String userType, String name, String password);
+	
+	
 	
 	
 }
