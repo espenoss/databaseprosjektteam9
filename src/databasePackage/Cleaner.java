0353056d98d52c08364292ad569372866d98d061
marith1 <marith1@student.hist.no>
2016-03-17 09:12:04 +0100

    Laget Cleaner klasse

---
+++ src/databasePackage/Cleaner.java
@@ -0,0 +1,29 @@
+package databasePackage;
+import java.sql.*;
+
+public class Cleaner {
+	
+	public static void closeResSet(ResultSet res){
+		
+	}
+	
+	public static void closeStatement(Statement stm){
+		
+	}
+	
+	public static void closeConnection(Connection con){
+		
+	}
+	
+	public static void rollback(Connection con){
+		
+	}
+	
+	public static void setAutoCommit(Connection con){
+		
+	}
+	
+	public static void writeMessage(Exception e, String message){
+		
+	}
+}
