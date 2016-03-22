52399cb0f23246bf0064250a0d711e88908ee6a1
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2016-03-22 14:23:32 +0100

    Jeg har begynt å lage GUI til login

--- src/GUI/LogIn.java
+++ src/GUI/LogIn.java
@@ -1,5 +1,78 @@
 package GUI;
 
-public class LogIn {
+import java.awt.BorderLayout;
 
+//lager vindu med to felter til utfylling(brukernavn og passord).
+//bruker GridLayout.
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+class LogIn extends JFrame{
+	private JTextField username = new JTextField(20);
+	private JTextField password = new JTextField(20);
+	
+	public LogIn(String title){
+		setTitle(title);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		UserInput input1 = new UserInput();
+		add(input1, BorderLayout.NORTH);
+		
+	    Button button = new Button();
+	    add(button, BorderLayout.SOUTH);	  
+	    pack();
+	}	    
+	    
+	/*
+	 * Lager en knapp
+	 * Lager en lytter og knytter knappen til denne lytteren
+	 */	  
+	    
+	private class Button extends JPanel{
+		public Button(){
+			Buttonlistener listener = new Buttonlistener();
+			JButton button = new JButton("Sign in");
+			add(button);
+		}
+	}
+	
+	/*
+	 * Lytter etter knappetrykk
+	 */
+	
+	private class Buttonlistener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+		}
+	}
+
+	/*
+	 * Argumentene til GridLayout() er:
+	 * antall rader, antall kolonner,
+	 * horisontal avstand mellom rutene og vertikal avstand mellom rutene
+	 * de to siste i antall piksler
+	 */
+	private class UserInput extends JPanel{
+		public UserInput(){
+			setLayout(new GridLayout(2,2,5,5));
+			JLabel text = new JLabel("Username: ", JLabel.RIGHT);
+			add(text);
+			add(username);
+			text = new JLabel("Password: ", JLabel.RIGHT);
+			add(text);
+			add(password);
+		}	
+	}
 }
+
+
+
+
