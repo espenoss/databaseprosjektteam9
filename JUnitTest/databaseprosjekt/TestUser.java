0c0f143f0170dfb9a8d67bc5c06159ebfa449260
anitakau <anitakau@stud.ntnu.no>
2016-04-06 10:21:48 +0200

    opprettet test

--- JUnitTest/databaseprosjekt/TestUser.java
+++ JUnitTest/databaseprosjekt/TestUser.java
@@ -1,14 +1,30 @@
 package databaseprosjekt;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TestUser {
+	
+	@Before
+	public void beforeTest(){
+		// opprett databaseforbindelse
+		
+		
+	}
+	
+	@After
+	public void afterTest(){
+		// lukk databaseforbindelse
+	}
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	
+	
 
 }
