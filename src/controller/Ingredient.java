9349326f201fa87e9c8cff4622b563b57fd92ec4
marith1 <marith1@student.hist.no>
2016-04-07 13:23:10 +0200

    class ingredient (controller)

--- src/controller/Ingredient.java
+++ src/controller/Ingredient.java
@@ -1,5 +1,36 @@
 package controller;
+import databasePackage.*;
 
 public class Ingredient {
-
+	public final int ingID; 
+	public String ingName;
+	public int quantity;
+	
+	public Ingredient(int ingID, String ingName, int quantity){
+		this.ingID = ingID;
+		this.ingName = ingName;
+		this.quantity = quantity;
+	}
+	
+	public int getIngID(){
+		return ingID;
+	}
+	
+	public String getIngName(){
+		return ingName;
+	}
+	
+	public int getQuantity(){
+		return quantity;
+	}
+	
+	public void setIngName(String ingName){
+		//endre info i databasen her?
+		this.ingName=ingName;
+	}
+	
+	public void setQuantity(int quantity){
+		//endre info i databasen her? 
+		this.quantity = quantity;
+	}
 }
