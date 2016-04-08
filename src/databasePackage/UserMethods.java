918cdee2d90877be6f814f346b25126fbb2a8b18
espen <espenoss@gmail.com>
2016-04-08 09:48:44 +0200

    a

--- src/databasePackage/UserMethods.java
+++ src/databasePackage/UserMethods.java
@@ -191,8 +191,12 @@ public class UserMethods {
 		return database.makeSingleStatement(statement);
 	}
 	
-	public static boolean addMealToPlan(int ){
+	public static boolean addMealToPlan(int subID, int mealID, int weekdayNr, String weekday, Database database) throws Exception{
 		
+		String statement = "INSERT INTO sub_meals_day VALUES(" + subID + ", " 
+		+ mealID + "," + weekdayNr + ", '" + weekday + "');";
+			
+		return database.makeSingleStatement(statement);
 	}
 	
 	
@@ -200,8 +204,8 @@ public class UserMethods {
 	public static void main(String[] args) throws Exception{
 		// testkode
 		
-		String username = "espenme";
-		String password = "16Sossosem06";
+		String username = "";
+		String password = "";
 		Database database = new Database("com.mysql.jdbc.Driver", "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/espenme?user=" + username + "&password=" + password);
 		String[][] resultat = null;
 		
