c38d81e30cfda4beed31420666dd4530003e4c9b
ninameed <meedby@gmail.com>
2016-04-10 13:51:51 +0200

    LogIn

--- src/controller/LogIn.java
+++ src/controller/LogIn.java
@@ -5,8 +5,8 @@ import databasePackage.*;
 public class LogIn {
 
 	String username = "espenme";
-	char[] password = {'1', '6', 'S', 'o', 's', 's', 'o', 's', 'e', 'm', '0', '6'};
-	String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + password;	
+	String passingword = "16Sossosem06";
+	String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + passingword;	
 	Database database = null;
 	public LogIn(){
 		database = new Database("com.mysql.jdbc.Driver", databasename);
