51727a0a316cb938d8e6ff8eaefaa25d9e959ab9
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2016-04-11 14:16:18 +0200

    gui

---
+++ src/controller/ViewAllCompanies.java
@@ -0,0 +1,18 @@
+package controller;
+
+import databasePackage.*;
+
+public class ViewAllCompanies{
+
+	String username = "espenme";
+	String passingword = "16Sossosem06";
+	String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + passingword;	
+	Database database = null;
+	public ViewAllCompanies(){
+		database = new Database("com.mysql.jdbc.Driver", databasename);
+	}
+	
+	public String[][] ViewAllCompaniesList(Database database) throws Exception{
+		return QueryMethods.viewAllCompanies(database);
+	}
+}
