eb90be963d3074c8da946129e6db35c10cd174c1
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2016-04-12 13:03:54 +0200

    gui

--- src/databaseguiii/MainSalesPerson.java
+++ src/databaseguiii/MainSalesPerson.java
@@ -8,64 +8,78 @@ import controller.*;
 import databasePackage.Database;
 
 class SalesPersonGui extends JFrame {
-	private Parentwindow parentwindow;
-	private MainCustomerGui mainCustomerGui;
-	private static final String [] CHOICES =
-		{"Register a new customer","Change customer information","View customer list","Register new food order","Change food order"};
-	private JList<String> choice_list = new JList<String>(CHOICES);  // Naa er listen laget!
+  private Parentwindow parentwindow;
+  private MainCustomerGui mainCustomerGui;
+  private static final String [] CHOICES =
+    {"Register new user","Change user information", "View all users", "Register new customer","Register new company",
+    		"Change customer information", "View private customers", "View company customers","Register new food order","Change food order"};
+  private JList<String> choice_list = new JList<String>(CHOICES);  // Naa er listen laget!
 
-	public SalesPersonGui(String tittel) {
-		setTitle(tittel);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-		JLabel ledetekst = new JLabel("Choose one of the following options.");
-		add(ledetekst, BorderLayout.NORTH);
+  public SalesPersonGui(String tittel) {
+    setTitle(tittel);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JLabel ledetekst = new JLabel("Choose one of the following options.");
+    add(ledetekst, BorderLayout.NORTH);
 
     /* Legger paa rullefelt */
-		JScrollPane rullefeltMedListe = new JScrollPane(choice_list);
-		add(rullefeltMedListe, BorderLayout.CENTER);
+    JScrollPane rullefeltMedListe = new JScrollPane(choice_list);
+    add(rullefeltMedListe, BorderLayout.CENTER);
 
-		ListeboksLytter lytter = new ListeboksLytter();
-    	choice_list.addListSelectionListener(lytter);
-    	pack();
-	}
+    ListeboksLytter lytter = new ListeboksLytter();
+    choice_list.addListSelectionListener(lytter);
+    pack();
+  }
 
-  /* Lytteren fanger opp alle klikk paa linjer i listeboksen */
-	private class ListeboksLytter implements ListSelectionListener {
-		private final Database database = null;
-		private Sales my_sales = new Sales("", 1, "", "", database);
-		private Customer my_customer = new Customer("","","","","",1,1,"");
-		private Meal my_meal = new Meal(1, "", "", true,1,1,1);
-		private Order my_order = new Order(1, "","", 1, "","");
-	
-		public void valueChanged(ListSelectionEvent hendelse) {
-			Object[] values = choice_list.getSelectedValuesList().toArray();
-			int choices = choice_list.getSelectedIndex();
-			if(choices == 0){
-				
-    	  //my_cook.();
-			}else if(choices==1){
-				try {
-				//	my_cook.markAsReadyForDelivery(my_meal, my_order);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-      
-			}else if(choices==2){
-				try {
-				//	my_cook.createMeal(my_meal);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		} 
-	}
-}
-  
-  
- class MainSalesPerson{
-  public static void main(String[] args) {
-	  SalesPersonGui window = new SalesPersonGui("Choose an option");
-	  window.setVisible(true);
+  /* Lytteren fanger opp alle klikk paa linjer i listeboksen */ 
+  private class ListeboksLytter implements ListSelectionListener {
+	private final Database database = null;
+	private QueryMethodsController queryMethodsController = new QueryMethodsController();
+	 private User viewAllUsers = new User("", 1, "", "", database);
+    public void valueChanged(ListSelectionEvent hendelse) {
+      Object[] values = choice_list.getSelectedValuesList().toArray();
+      int choices = choice_list.getSelectedIndex();
+      if(choices == 0){
+    	  TestUserDialog.main(null);
+      }else if(choices==1){
+    	  ////metoden for å endre info til en bruker
+      }
+      else if(choices==2){
+    	  try {
+			queryMethodsController.viewUserList();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      	} else if(choices == 3){
+    	  MainCustomerGui.main(null);
+      }else if(choices==4){
+    	  //metoden for a registrere en bedrift
+      }else if(choices==5){
+    	  //metoden for a endre info til en kundre
+      }
+      	else if(choices==6){
+    	  queryMethodsController.ViewAllCustomersList();
+		}
+      else if(choices==7){
+    	  try {
+			queryMethodsController.ViewAllCompaniesList();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      }else if(choices==8){
+    	  //metoden for a registrere en ny bestilling
+      }else if(choices==9){
+    	  //motoden for a endre bestilling
+      }
+    }
   }
 }
+
+ class MainSalesPerson {
+  public static void main(String[] args) {
+	  SalesPersonGui etVindu = new SalesPersonGui("Choose an option");
+    etVindu.setVisible(true);
+  }
+ }
