c463310296f764413f8c32f2d0e21d8e18cd6ce4
anitakau <anitakau@stud.ntnu.no>
2016-04-14 12:41:03 +0200

    inititate database

--- src/databasePackage/Database.java
+++ src/databasePackage/Database.java
@@ -142,7 +142,7 @@ public class Database {
 	}
 	
 	
-	public boolean initiateDb(String table, String createTable){
+	public boolean initiateDb(){
         boolean ok = false;
         Connection connection = null;                  
     	Statement statement = null;
@@ -151,8 +151,39 @@ public class Database {
         	Class.forName(dbDriver);        
         	connection = DriverManager.getConnection(dbName);                    
         	statement = connection.createStatement();
-        	String sqlDeleteTable = "DROP TABLE IF EXISTS "+table;
-	        String sqlCreateTable = createTable;
+        	String sqlDeleteTable = "DROP TABLE IF EXISTS sub_meals_day; "
+        			+ "DROP TABLE IF EXISTS ordered_meal; "
+        			+ "DROP TABLE IF EXISTS meal_ingredient; "
+        			+ "DROP TABLE IF EXISTS ingredient; "
+        			+ "DROP TABLE IF EXISTS sub_order; "
+        			+ "DROP TABLE IF EXISTS subscription_plan; "
+        			+ "DROP TABLE IF EXISTS meal; "
+        			+ "DROP TABLE IF EXISTS food_order; "
+        			+ "DROP TABLE IF EXISTS company; "
+        			+ "DROP TABLE IF EXISTS customer; "
+        			+ "DROP TABLE IF EXISTS user; "
+        			+ "DROP TABLE IF EXISTS zone;";
+	        String sqlCreateTable = 
+	        	"CREATE TABLE zone(zone_nr INTEGER NOT NULL, zone_name VARCHAR(30) NOT NULL, CONSTRAINT zone_pk PRIMARY KEY (zone_nr));"
+	        	+"CREATE TABLE user(user_id VARCHAR(20) NOT NULL, user_type INTEGER NOT NULL, name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, CONSTRAINT user_pk PRIMARY KEY (user_id));"
+	        	+"CREATE TABLE customer(customer_id INTEGER NOT NULL, surname VARCHAR(30), firstname VARCHAR(30), phone_number CHAR(8),email VARCHAR(50), adress VARCHAR(100), zip_code  INTEGER NOT NULL, zone_nr  INTEGER NOT NULL, preferences VARCHAR(100), active BOOLEAN, CONSTRAINT customer_pk PRIMARY KEY (customer_id));"
+	        	+"CREATE TABLE company(customer_id INTEGER NOT NULL, company_name VARCHAR(30), CONSTRAINT company_pk PRIMARY KEY (customer_id));"
+	        	+"CREATE TABLE food_order(order_id INTEGER NOT NULL, order_date DATE NOT NULL, customer_id INTEGER NOT NULL, info VARCHAR(200), user_id VARCHAR(20) NOT NULL, CONSTRAINT order_pk PRIMARY KEY (order_id));"
+	        	+"CREATE TABLE sub_order(order_id INTEGER NOT NULL, quantity_sub INTEGER NOT NULL, from_date DATE NOT NULL, to_date DATE, sub_id INTEGER, CONSTRAINT sub_order_pk PRIMARY KEY(order_id));"
+	        	+"CREATE TABLE subscription_plan(sub_id INTEGER NOT NULL, sub_name VARCHAR(30) NOT NULL, CONSTRAINT sub_plan_pk PRIMARY KEY(sub_id));"
+	        	+"CREATE TABLE meal(meal_id INTEGER NOT NULL, meal_name VARCHAR(30), instructions VARCHAR(200), available BOOLEAN, price INTEGER, CONSTRAINT meal_pk PRIMARY KEY (meal_id));"
+	        	+"CREATE TABLE ingredient(ingredient_id INTEGER NOT NULL, name VARCHAR(30), quantity FLOAT, unit VARCHAR(30), CONSTRAINT ingredient_pk PRIMARY KEY (ingredient_id));"
+	        	+"CREATE TABLE meal_ingredient(meal_id INTEGER NOT NULL, ingredient_id INTEGER NOT NULL, quantity FLOAT NOT NULL, CONSTRAINT meal_ingredient_pk PRIMARY KEY(meal_id, ingredient_id));"
+	        	+"CREATE TABLE ordered_meal(order_id INTEGER NOT NULL, meal_id INTEGER NOT NULL, delivery_date DATE NOT NULL, quantity INTEGER NOT NULL, ready_delivery BOOLEAN NOT NULL, delivered BOOLEAN NOT NULL, CONSTRAINT ordered_meal_pk PRIMARY KEY(order_id, meal_id, delivery_date));"
+	        	+"CREATE TABLE sub_meals_day( sub_id INTEGER NOT NULL, meal_id INTEGER NOT NULL, weekday_nr INTEGER NOT NULL, weekday VARCHAR(10), CONSTRAINT sub_meals_day_pk PRIMARY KEY(sub_id, meal_id, weekday_nr));"
+	        	+"ALTER TABLE customer ADD CONSTRAINT customer_fk1 FOREIGN KEY(zone_nr) REFERENCES zone (zone_nr);"
+	        	+"ALTER TABLE company ADD CONSTRAINT company_fk1 FOREIGN KEY(customer_id) REFERENCES customer(customer_id);"
+	        	+"ALTER TABLE food_order ADD CONSTRAINT order_fk1 FOREIGN KEY(user_id) REFERENCES user(user_id);"
+	        	+"ALTER TABLE food_order ADD CONSTRAINT order_fk2 FOREIGN KEY(customer_id) REFERENCES customer (customer_id);"
+	        	+"ALTER TABLE sub_order ADD CONSTRAINT sub_order_fk1 FOREIGN KEY(order_id) REFERENCES food_order(order_id), ADD CONSTRAINT sub_order_fk2 FOREIGN KEY(sub_id) REFERENCES subscription_plan(sub_id);"
+	        	+"ALTER TABLE meal_ingredient ADD CONSTRAINT meal_ingredient_fk1 FOREIGN KEY(meal_id) REFERENCES meal(meal_id), ADD CONSTRAINT meal_ingredient_fk2 FOREIGN KEY(ingredient_id) REFERENCES ingredient(ingredient_id);"
+	        	+"ALTER TABLE ordered_meal ADD CONSTRAINT ordered_meal_fk1 FOREIGN KEY(order_id) REFERENCES food_order(order_id), ADD CONSTRAINT ordered_meal_fk2 FOREIGN KEY(meal_id) REFERENCES meal(meal_id);"
+	        	+"ALTER TABLE sub_meals_day ADD CONSTRAINT sub_meals_day_fk1 FOREIGN KEY(sub_id) REFERENCES subscription_plan(sub_id), ADD CONSTRAINT sub_meals_day_fk2 FOREIGN KEY(meal_id) REFERENCES meal(meal_id);";
 	        statement.executeUpdate(sqlDeleteTable );
 	        statement.executeUpdate(sqlCreateTable);
         	ok = true;
