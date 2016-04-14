99eb4f7230df5fe4632c59003840ba6476c4f025
espen <espenoss@gmail.com>
2016-04-14 10:50:18 +0200

    date

--- src/controller/Order.java
+++ src/controller/Order.java
@@ -93,4 +93,13 @@ public class Order {
 		
 	}
 	
+	static public void main(String[] arg){
+		
+	    java.util.Date utilDate = new java.util.Date();
+	    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+		
+		System.out.println(sqlDate);
+		
+	}
+	
 }
