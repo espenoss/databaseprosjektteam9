e49b136636921f9ac755d4b8345dc54e9add12d3
anitakau <anitakau@stud.ntnu.no>
2016-04-15 15:34:00 +0200

    test customer

--- JUnitTest/testController/TestCustomer.java
+++ JUnitTest/testController/TestCustomer.java
@@ -1,17 +1,19 @@
 package testController;
 
-import databasePackage.QueryMethods;
+import databasePackage.Database;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import controller.Customer;
 import databasePackage.Database;
+import databasePackage.QMCustomer;
 
 public class TestCustomer {
 	private static Database database;
-	private Customer customer;
+	private static Customer customer;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,7 +24,7 @@ public class TestCustomer {
 		
 		database = new Database("com.mysql.jdbc.Driver", databasename);
 		database.initiateDb();
-		customer = new Customer(1, "Jens", "Svendsen", "12345678", "", String adress, int zipCode, int zoneNr, String preferences, boolean active){
+		customer = new Customer(1, "Jens", "Svendsen", "12345678", "jens@svendsen.com", "Veibakken 12", 7014, 1, "None", true);
 	}
 	
 	@Before
@@ -30,8 +32,13 @@ public class TestCustomer {
 	}
 
 	@Test
-	public void testShouldUpdateCustomer() {
+	public void testShouldUpdateCustomer() throws Exception{
 		//boolean updateCustomer(Database database)
+		boolean res = customer.updateCustomer(database);
+		boolean expRes = true;
+		
+		assertEquals(res, expRes);
+	
 	}
 
 }
