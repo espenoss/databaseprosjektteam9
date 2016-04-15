19f232af786224b482b4defe17a32764749b885d
anitakau <anitakau@stud.ntnu.no>
2016-04-15 21:57:25 +0200

    test LogIn

--- JUnitTest/testController/TestLogIn.java
+++ JUnitTest/testController/TestLogIn.java
@@ -1,24 +1,61 @@
 package testController;
 
+import databasePackage.QMUser;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import controller.LogIn;
+import controller.User;
+import databasePackage.Database;
+import databasePackage.QMUser;
+
 public class TestLogIn {
+	private static Database database;
+	LogIn login;
+	private static char[] pword = {'1','2','3','4'};
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		String username = "anitakau";
+		String password = "e82L3Dat";
+		
+		String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + password;
+		
+		database = new Database("com.mysql.jdbc.Driver", databasename);
+		database.initiateDb();
+		
+		String insert = "INSERT INTO user VALUES('trym123',1,'Trym Hansen',1234)";
+		database.makeSingleStatement(insert);
+	
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		login = new LogIn();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testShouldLogIn() throws Exception{
+		System.out.println("Test: log in");
+		
+		int res = login.logIn("trym123", pword);
+		int expRes = 1;
+		
+		assertEquals(res, expRes);
+		
+	}
+	@Test
+	public void testShouldFailLogIn() throws Exception{
+		System.out.println("Test: fail log in");
+		
+		int res = login.logIn("trine43", pword);
+		int expRes = -1;
+		
+		assertEquals(res, expRes);
 	}
 
 }
--- src/controller/LogIn.java
+++ src/controller/LogIn.java
@@ -3,11 +3,13 @@ package controller;
 import databasePackage.*;
 
 public class LogIn {
+	String username = "anitakau";
+	String passingword = "e82L3Dat";
 
-	String username = "espenme";
-	String passingword = "16Sossosem06";
+//	String username = "espenme";
+//	String passingword = "16Sossosem06";
 	String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + passingword;	
-	Database database = null;
+	private Database database;
 	public LogIn(){
 		database = new Database("com.mysql.jdbc.Driver", databasename);
 	}
