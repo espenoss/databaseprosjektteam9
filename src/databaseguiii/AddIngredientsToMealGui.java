c768aee4da01ca51379dff41caca0faf45e8d537
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2016-04-21 08:43:17 +0200

    AddIngredientsToMealGui

--- src/databaseguiii/AddIngredientsToMealGui.java
+++ src/databaseguiii/AddIngredientsToMealGui.java
@@ -20,82 +20,111 @@ import javax.swing.event.ListSelectionListener;
 import controller.*;
 import databasePackage.*;
 
- class AddIngredientsToMeal extends JFrame {
-	 	private Database database = new Database ("com.mysql.jdbc.Driver", "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/espenme?user=espenme&password=16Sossosem06");
-	 	Cook cook = new Cook("", "", database);
-	 
+ class AddIngredientsToMealGui extends JFrame {
+	 	//private Database database = new Database ("com.mysql.jdbc.Driver", "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/espenme?user=espenme&password=16Sossosem06");
+	 	Cook cook = null;
+	 	
+	 	
+	 	public AddIngredientsToMealGui(Cook cook){
+	 		this.cook = cook;
+	 		AddIngredientsDialog dialog = new AddIngredientsDialog(this);
+	 		dialog.setVisible(true);
+	  		setTitle("Choose the meal and its ingrediets");
+	  		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  		setLayout(new FlowLayout());
+	  		setLocation(300, 300); 
+	  		dialog.setLocation(350, 350); 
+	 	}
 	
-	 private class AddIngredients extends MyDialog {
-		 private TextEditor editor = new TextEditor();
-		 private ArrayList<Meal> mealList = new ArrayList<Meal>();
-		 private JComboBox mealIdSelected;
-		 private ArrayList<Ingredient> ingredientsList = new ArrayList<Ingredient>();
-		 private JComboBox ingredientSelected;
-		 private JTextField ingredient_quantityField = new JTextField(20);
-		 private float myQuantity;
-		 int mealIndex;
-		 int ingredientIndex;
+	 	private class AddIngredientsDialog extends MyDialog {
+	 		private TextEditor editor = new TextEditor();
+	 		
+	 		private ArrayList<Meal> mealList = new ArrayList<Meal>();
+	 		private JComboBox mealIdSelected;
+	 		
+	 		private ArrayList<Ingredient> ingredientsList = new ArrayList<Ingredient>();
+	 		private JComboBox ingredientSelected;
+	 		
+	 		private JTextField ingredient_quantityField = new JTextField(20);
+	 		
+	 		private float myQuantity;
+	 		int mealIndex;
+	 		int ingredientIndex;
 		 
 			
 		 
-		 public AddIngredients(JFrame parent){
-			 super(parent, "Select ingredients ");
-			 add(new JPanel(), BorderLayout.NORTH);
-			 add(new IngredientsDatapanel(),BorderLayout.CENTER);
-			 add(getButtonPanel(),BorderLayout.SOUTH);
-			 pack();
-		}
+	 		public AddIngredientsDialog(JFrame parent){
+	 			super(parent, "Select ingredients ");
+	 			add(new JPanel(), BorderLayout.NORTH);
+	 			add(new IngredientsDatapanel(),BorderLayout.CENTER);
+	 			add(getButtonPanel(),BorderLayout.SOUTH);
+	 			pack();
+	 		}
 		   
-		 private class IngredientsDatapanel extends JPanel{
-			public IngredientsDatapanel(){
-				setLayout(new GridLayout(3,2));
+	 		private class IngredientsDatapanel extends JPanel{
+	 			public IngredientsDatapanel(){
+	 				setLayout(new GridLayout(3,2));
+	 				try {
+		 				mealList = cook.viewAvailableMeals();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					ArrayList<String> my_list = new ArrayList<>();
+					for(Meal m: mealList){
+						my_list.add(m.getMealName());
+					}
+					mealIdSelected = new JComboBox<>(my_list.toArray());
+	 				add(new JLabel("Meal Id: ", JLabel.RIGHT));
+	 				add(mealIdSelected);
+	 				
+	 				
+	 				try {
+						ingredientsList = cook.viewIngredients();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					ArrayList<String> my_ingr_list = new ArrayList<>();
+					for(Ingredient i: ingredientsList){
+						my_ingr_list.add(i.getIngName());
+					}
+					ingredientSelected = new JComboBox<>(my_ingr_list.toArray());
+					
+	 				add(new JLabel("Ingredient Id: ", JLabel.RIGHT));
+	 				add(ingredientSelected);
 				
-				add(new JLabel("Meal Id: ", JLabel.RIGHT));
-				add(mealIdSelected);
-
-				add(new JLabel("Ingredient Id: ", JLabel.RIGHT));
-				add(ingredientSelected);
-				
-				add(new JLabel("Ingredient quantity: ", JLabel.RIGHT));
-				add(ingredient_quantityField);
-			}
-		}
+	 				add(new JLabel("Ingredient quantity: ", JLabel.RIGHT));
+	 				add(ingredient_quantityField);
+	 			}
+	 		}
 	
-		 public boolean okData(){
-			 mealIndex = mealIdSelected.getSelectedIndex();
-			 Meal currMeal = mealList.get(mealIndex);
-			 ingredientIndex = ingredientSelected.getSelectedIndex();
-			 Ingredient currIngredient = ingredientsList.get(ingredientIndex);
-		 
-			 String ingredient_quantity = ingredient_quantityField.getText();
-			 float my_quantity = editor.stringToFloat(ingredient_quantity);
-			 myQuantity = my_quantity;
+	 		public boolean okData(){
+	 			mealIndex = mealIdSelected.getSelectedIndex();
+	 			ingredientIndex =ingredientSelected.getSelectedIndex();
+	 			
+	 			String ingredient_quantity = ingredient_quantityField.getText();
+	 			float my_quantity = editor.stringToFloat(ingredient_quantity);
+			 	myQuantity = my_quantity;
 	 
-			 try {
-				 cook.addIngredientToMeal(mealIndex, ingredientIndex, myQuantity) ;
-			 }catch (Exception e) {
-				 System.out.println(e.toString());
-			 }
-			 return true;
-		 }
-	 }
-	 public AddIngredientsToMeal(){ 
-		 AddIngredients addIngredients = new AddIngredients(this);
-		 addIngredients.setVisible(true);
-		 setTitle("Add ingredients");
-		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 setLayout(new FlowLayout());
-		 setLocation(300, 300); 
-		 addIngredients.setLocation(350, 350);  	 
-	 }	 
-}
- 
- class AddIngredientsToMealGui {
-	 static public void main(String[] args) {
-		 AddIngredientsToMeal test = new AddIngredientsToMeal();
-	 }   
-} 
-		
+	 
+			 	try {
+			 		cook.addIngredientToMeal(mealIndex, ingredientIndex, myQuantity) ;
+			 	}catch (Exception e) {
+			 		System.out.println(e.toString());
+			 	}
+			 	return true;
+	 		}
+	 	}
+	
+	 	public static void main(String[] args){
+	 		String username = "espenme";
+	 		String passingword = "16Sossosem06";
+	 		String databasename = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/" + username + "?user=" + username + "&password=" + passingword;	
+	 		Database database = new Database("com.mysql.jdbc.Driver", databasename);
+			new AddIngredientsToMealGui(new Cook("","", database));
+	 	}
+ }  
 		
 		  
  
