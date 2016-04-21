af25be35fb059a48fe74b1b92b7fe128292b0163
Marit Holm <marith1@stud.hist.no>
2016-04-21 11:56:12 +0200

    endret textEditor

--- src/controller/TextEditor.java
+++ src/controller/TextEditor.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
 public class TextEditor {
 	
 	public TextEditor(){
@@ -7,38 +9,45 @@ public class TextEditor {
 
 	public int stringToInt(String text){ // removes blank spaces and parses string to an int
 		int i = 0;
-		if(!text.equals(null)){
-			text.trim();
+		text.trim();
+
+		if(text == null || text.isEmpty()){
+			JOptionPane.showMessageDialog(null,"Input is empty.");
+		}else{
 			try{
 				i = Integer.parseInt(text);
+				return i;
 			} catch (NumberFormatException e) {
-				System.out.println("Input not an int");
+				JOptionPane.showMessageDialog(null, "Input is not an integer.");
 			}
 		}
-		return i;
+		return -1;
 	}
 	
 	public float stringToFloat(String text){ // removes blank spaces and parses a string to a float
 		float i = 0;
-		if(!text.equals(null)){
-			text.trim();
+		text.trim();
+		
+		if(text == null || text.isEmpty()){
+			JOptionPane.showMessageDialog(null,"Input is empty.");
+		}else{
 			try{
 				i = Float.parseFloat(text);
+				return i;
 			} catch (NumberFormatException e){
-				System.out.println("Input not a float");
+				JOptionPane.showMessageDialog(null,"Input not a float");
 			}
-		} else {
-			System.out.println("Please enter a text");
 		}
-		return i;
+		return -1;
 	}
+	
 	
 	public String trimAndLowercase(String text){ // removes blank spaces and sets characters to lowercase
 		if(!text.equals(null)){
 			text.trim();
 			text.toLowerCase();
 		} else {
-			System.out.println("Please enter a text");
+			JOptionPane.showMessageDialog(null,"Please enter a text");
 		}
 		return text;
 	}
@@ -47,8 +56,21 @@ public class TextEditor {
 		if(!text.equals(null)){
 			text.trim();
 		} else{
-			System.out.println("Please enter a text");
+			JOptionPane.showMessageDialog(null,"Please enter a text");
 		}
 		return text;
+	}
+	
+	public static void main(String[] args){
+		TextEditor t = new TextEditor();
+		
+		System.out.println("float ok: "+t.stringToFloat("1.2"));
+		System.out.println("float not ok: "+t.stringToFloat("hei"));
+		System.out.println("float empty: "+t.stringToFloat(""));
+		
+		System.out.println("int ok: "+t.stringToInt("1"));
+		System.out.println("int not ok: "+t.stringToInt("hei"));
+		System.out.println("int empty: "+t.stringToInt(""));
+		
 	}
 }
