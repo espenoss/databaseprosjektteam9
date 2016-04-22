204fdff0cc6d1966c9b9f7599935cdae34418024
anitakau <anitakau@stud.ntnu.no>
2016-04-22 15:52:34 +0200

    TestSubPlan finished

--- JUnitTest/testController/TestSubPlan.java
+++ JUnitTest/testController/TestSubPlan.java
@@ -9,12 +9,16 @@ import org.junit.Test;
 
 import controller.Meal;
 import controller.SubPlan;
+import controller.TextEditor;
 import databasePackage.Database;
+import databasePackage.QMFood;
 
 public class TestSubPlan {
 	private static Database database;
 	private static SubPlan instance;
-	private static Meal meal;
+	private static Meal[] meals = new Meal[7];
+	private static Meal meal1;
+	private static Meal meal3;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -26,9 +30,28 @@ public class TestSubPlan {
 		database = new Database("com.mysql.jdbc.Driver", databasename);
 		database.initiateDb();
 		
-		instance = new SubPlan(1, "name"); //subPlanID, name
-		meal = new Meal(1, "spaghetti", "none", true, 100);
+		
+		String insert1 = "INSERT INTO meal VALUES(1, 'spaghetti', NULL, true, 100)";
+		String insert2 = "INSERT INTO meal VALUES(2, 'hamburger', NULL, true, 100)";
+		
+		String insert3 = "INSERT INTO subscription_plan VALUES(1, 'Dinner plan')";
+		String insert4 = "INSERT INTO sub_meals_day VALUES(1,1,1,'monday')";
+		String insert5 = "INSERT INTO sub_meals_day VALUES(1,2,3,'wednesday')";
+		
+		database.makeSingleStatement(insert1);
+		database.makeSingleStatement(insert2);
+		database.makeSingleStatement(insert3);
+		database.makeSingleStatement(insert4);
+		database.makeSingleStatement(insert5);
+		
+		
+		instance = new SubPlan(1, "Dinner plan"); //subPlanID, name
+		meal1 = new Meal(1, "spaghetti", "none", true, 100);
+		meal3 = new Meal(2, "hamburger", "none", true, 100);
 	
+		meals[0] = meal1;
+		meals[2] = meal3;
+		
 	}
 	
 	@Before
@@ -40,7 +63,7 @@ public class TestSubPlan {
 	public void testShouldAddMealToSubPlan(){
 		System.out.println("SubPlan test 1: add meal to subplan");
 		
-		boolean res = instance.addMealToSubPlan(meal, 2);
+		boolean res = instance.addMealToSubPlan(meal1, 2);
 		boolean expRes = true;
 		
 		assertEquals(res, expRes);
@@ -50,7 +73,7 @@ public class TestSubPlan {
 	public void testShouldFailAddMealToSubPlan(){
 		System.out.println("SubPlan test 2: fail add meal to subplan");
 		
-		boolean res = instance.addMealToSubPlan(meal,9);
+		boolean res = instance.addMealToSubPlan(meal1,9);
 		boolean expRes = false;
 		
 		assertEquals(res, expRes);
@@ -83,7 +106,22 @@ public class TestSubPlan {
 		boolean res = instance.updateSubPlan(database);
 		boolean expRes = true;
 		
+		assertEquals(res, expRes);	
+	}
+
+	@Test
+	public void testShouldFetchMeals() throws Exception{
+		System.out.println("SubPlan test 6: Fetch meals in plan from database");
+	
+		instance.fetchMealsInPlan(database);
+		meals = instance.getMeals();
+		
+		boolean res = meals[0].equals(meal1);
+		boolean expRes = true;
 		assertEquals(res, expRes);
 		
+		boolean res2 = meals[2].equals(meal3);
+		boolean expRes2 = true;
+		assertEquals(res2, expRes2);
 	}
 }
